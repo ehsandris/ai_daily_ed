f95b02707aba43c0dfbcbdbd475e008accd6c024
EhsanDris <ehsanderisavi@gmail.com>
2026-08-03 13:50:53 +0330

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 
 # تنظیم هوش مصنوعی
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 def get_latest_ai_news():
     print("در حال دریافت اخبار از TechCrunch...")
